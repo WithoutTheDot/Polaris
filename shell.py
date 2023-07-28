474fea6ba55d7fea1fac3afcb7dd3f7e343ca48a
WithoutTheDot <shieldsjoshua2402@gmail.com>
2023-07-28 18:12:44 +0100

    Basic Arithmetic Working

--- shell.py
+++ shell.py
@@ -3,6 +3,7 @@ import basic
 
 while True:
     text = input(">> ")
+    if text == "QUIT!": break
     result, error = basic.run("<stdin>", text)
     if error: print(error.as_string())
     else: print(result)
