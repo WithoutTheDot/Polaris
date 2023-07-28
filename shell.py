ed7882f6932298f7cbf74920f5fd5c2ad4193a27
WithoutTheDot <shieldsjoshua2402@gmail.com>
2023-07-28 13:11:49 +0100

    Basic Tokens and Errors

--- shell.py
+++ shell.py
@@ -1,5 +1,8 @@
+import basic
+
 
 while True:
     text = input(">> ")
-    print(text)
-    
+    result, error = basic.run("<stdin>", text)
+    if error: print(error.as_string())
+    else: print(result)
